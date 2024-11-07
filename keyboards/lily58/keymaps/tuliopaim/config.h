1d70796627c4438f1ea32fb2df0a992d78a52e49
tuliopaim <tutpaim@gmail.com>
2024-11-07 18:36:11 -0300

    add homerow mods

--- keyboards/lily58/keymaps/tuliopaim/config.h
+++ keyboards/lily58/keymaps/tuliopaim/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+// Homerow mod timeout settings (in milliseconds)
+#define TAPPING_TERM 200
 
 // Underglow
 /*
